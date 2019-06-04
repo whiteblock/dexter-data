845331fe2b37847660d62f5f2e240df76b51a187
John BEPPU <john.beppu@gmail.com>
2019-06-04 09:48:39 -0700

    Sketch out requirements

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-const world = '️';
+import 'ccxt'
 
-export function hello(word: string = world): string {
-  return `Hello ${word}!!`;
-}
+/*
+
+  Requirements
+
+  - Provide a way to ask for candlestick data from an exchange and market and timeframe.
+    + If candlesticks of a certain timeframe are not available, try to derive them from smaller timeframes that *are* available.
+  - Provide a way to start and stop continuous download of candlesticks for a market.
+  - Keep metadata on what markets I have data for.
+  - Provide a way to stream candlesticks out to consumers.
+    + Optionally, clean the candlestick data if they ask.
+  - Wrap all this in a gRPC service.
+
+ */
+
