5fad102216d9233c18e1f1b4c15945d6fcfa339f
John BEPPU <john.beppu@gmail.com>
2019-06-07 10:12:24 -0700

    Add some preliminary functions for getting exchange data
    - supportedExchanges
    - suppportedMarkets
    - getCandles

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import 'ccxt'
+import ccxt from 'ccxt';
+import Bookshelf from 'bookshelf';
+import Knex from 'knex';
+import Lazy from 'lazy.js';
 
 /*
 
   Requirements
 
   - Provide a way to ask for candlestick data from an exchange and market and timeframe.
-    + If candlesticks of a certain timeframe are not available, try to derive them from smaller timeframes that *are* available.
+    + If candlesticks of a certain timeframe are not available,
+      try to derive them from smaller timeframes that *are* available.
   - Provide a way to start and stop continuous download of candlesticks for a market.
   - Keep metadata on what markets I have data for.
   - Provide a way to stream candlesticks out to consumers.
@@ -14,3 +18,40 @@ import 'ccxt'
 
  */
 
+function supportedExchanges() {
+  return Lazy(ccxt.exchanges)
+    .map((name: string) => new ccxt[name]())
+    .filter((ex: any) => ex.has['fetchOHLCV'])
+    .map((ex: any) => ex.id)
+    .toArray()
+}
+
+async function supportedMarkets(exchange: string) {
+  const ex: any = new ccxt[exchange]();
+  const markets = await ex.loadMarkets();
+  return markets;
+}
+
+async function getCandles(exchange: string, market: string, timeframe: string) {
+  const ex: any = new ccxt[exchange]();
+  const candles: Array<Object> = await ex.fetchOHLCV(market, timeframe);
+  return candles
+}
+
+async function streamCandles(exchange: string, market: string, timeframe: string) {
+}
+
+function archiveMarket(exchange: string, market: string, timeframe: string) {
+}
+
+export default {
+  Bookshelf,
+  Knex,
+  ccxt,
+  Lazy,
+  supportedExchanges,
+  supportedMarkets,
+  getCandles,
+  streamCandles,
+  archiveMarket,
+};
