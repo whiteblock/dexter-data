fd78b1a5afbbf35f369e0e71ec2b26427f5ba2ad
John BEPPU <john.beppu@gmail.com>
2019-06-11 19:00:28 -0700

    Let EventEmitter and luxon be available in the repl

--- .repl.js
+++ .repl.js
@@ -14,6 +14,12 @@ global.Lazy        = require('lazy.js')
 global.sprintf     = require('sprintf')
 global.outdent     = require('outdent')
 global.ccxt        = require('ccxt')
+global.luxon       = require('luxon')
+global.DateTime    = luxon.DateTime
+global.Interval    = luxon.Interval
+global.events      = require('events')
+global.EventEmitter = events.EventEmitter
 
 global.dd          = require('./dist').default // dexter-data
 global.service     = require('./dist/service').default
+global.time        = require('./dist/time').default
